07239551b52b3eb6fb53e87d93f0a4b8d9f20db9
dearian <dearian2706@gmail.com>
2024-07-31 18:39:25 +0300

    Add first font through Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,17 @@ export default {
       boxShadow: {
         "3d": "0 4px 0",
       },
+      fontFamily: {
+        mono: ["Roboto Mono", "monospace"],
+      },
+      colors: {
+        dark: "#302419",
+        light: "#D3C2B2",
+        accent: "#CC141C",
+        main: "#F9F5F1",
+        // theme: {
+        // },
+      },
     },
   },
   plugins: [],
