0abc4f1eb02a8f780d99e1f5b2845c674305237f
dearian <dearian2706@gmail.com>
2024-08-03 15:37:16 +0300

    Comment isVoting

--- src/pages/Game/index.tsx
+++ src/pages/Game/index.tsx
@@ -52,7 +52,7 @@ const Game = () => {
     selectedPlayers,
     day,
     submitSelection,
-    voting: { isVoting },
+    // voting: { isVoting },
     additionalSettings: { currentCycle },
   } = useSelector((state: RootState) => state.game.game);
   const dispatch = useDispatch();
@@ -96,63 +96,63 @@ const Game = () => {
       </PhaseLayout>
 
       <PhaseLayout dayPhase="day">
-        {isVoting ? (
+        {/* {isVoting ? (
           <div>voting</div>
-        ) : (
-          <div>
-            <header className="w-full bg-gray-100 p-4 flex justify-between">
-              <h1 className="text-md font-bold">День: {day}</h1>
-              <button
-                ref={buttonRef}
-                onClick={() => setOpenMenu((prev) => !prev)}
-              >
-                Меню
-              </button>
-            </header>
-            <div className="flex-1 flex flex-col">
-              <div className="flex bg-blue-200 self-start">
-                <input
-                  readOnly
-                  value={blueTeamPoints}
-                  type="number"
-                  className="h-16 w-16 bg-transparent text-center text-[1.4rem] font-bold text-blue-400"
-                />
-                <div className="flex flex-col bg-blue-300">
-                  <button
-                    className="h-8 w-8 font-bold"
-                    onClick={() => setBlueTeamPoints((prev) => prev + 1)}
-                  >
-                    +
-                  </button>
-                  <button
-                    className="h-8 w-8 font-bold"
-                    onClick={() =>
-                      setBlueTeamPoints((prev) => (prev ? prev - 1 : prev))
-                    }
-                  >
-                    -
-                  </button>
-                </div>
-              </div>
-              <SpeechTimer />
-
-              <div className="flex flex-col gap-2 self-center items-center my-4 p-2 border border-dark rounded-md">
-                <h1 className="text-xl font-bold">Голосування</h1>
-                <PlayersList maxSelected={players?.length} />
-                {/* //* додати день під результатом голосування */}
-                <Button
-                  // disabled={} //* disable якщо не було місії у цьому ходу або модалку висвітити при спробі
-                  onClick={() => dispatch(setIsVoting(true))}
-                  className="bg-cyan-400 shadow-cyan-500"
+        ) : ( */}
+        <div>
+          <header className="w-full bg-gray-100 p-4 flex justify-between">
+            <h1 className="text-md font-bold">День: {day}</h1>
+            <button
+              ref={buttonRef}
+              onClick={() => setOpenMenu((prev) => !prev)}
+            >
+              Меню
+            </button>
+          </header>
+          <div className="flex-1 flex flex-col">
+            <div className="flex bg-blue-200 self-start">
+              <input
+                readOnly
+                value={blueTeamPoints}
+                type="number"
+                className="h-16 w-16 bg-transparent text-center text-[1.4rem] font-bold text-blue-400"
+              />
+              <div className="flex flex-col bg-blue-300">
+                <button
+                  className="h-8 w-8 font-bold"
+                  onClick={() => setBlueTeamPoints((prev) => prev + 1)}
                 >
-                  Розпочати
-                </Button>
+                  +
+                </button>
+                <button
+                  className="h-8 w-8 font-bold"
+                  onClick={() =>
+                    setBlueTeamPoints((prev) => (prev ? prev - 1 : prev))
+                  }
+                >
+                  -
+                </button>
               </div>
             </div>
+            <SpeechTimer />
 
-            <Missions />
+            <div className="flex flex-col gap-2 self-center items-center my-4 p-2 border border-dark rounded-md">
+              <h1 className="text-xl font-bold">Голосування</h1>
+              <PlayersList maxSelected={players?.length} />
+              {/* //* додати день під результатом голосування */}
+              <Button
+                // disabled={} //* disable якщо не було місії у цьому ходу або модалку висвітити при спробі
+                onClick={() => dispatch(setIsVoting(true))}
+                className="bg-cyan-400 shadow-cyan-500"
+              >
+                Розпочати
+              </Button>
+            </div>
           </div>
-        )}
+
+          <Missions />
+        </div>
+        {/* )} */}
       </PhaseLayout>
 
       <BottomPanel className="justify-end">
